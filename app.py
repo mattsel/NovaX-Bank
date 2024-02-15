ad1c822c7ed6c3c15163cc3993992c26f7f2c57a
mattsel <matt.sel04@gmail.com>
2024-02-15 16:41:28 -0500

    Updated Connection Strings

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ load_dotenv(dotenv_path)
  
 app = Flask(__name__, static_url_path='/static')
 # Retrieve database connection details from environment variables
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
+#DB_USER = os.environ.get('DB_USER')
+#DB_PASSWORD = os.environ.get('DB_PASSWORD')
+#DB_HOST = os.environ.get('DB_HOST')
+#DB_NAME = os.environ.get('DB_NAME')
 
 # Construct the SQLALCHEMY_DATABASE_URI without specifying the driver
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
