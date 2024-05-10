1f754d7d6b101b54f8fdecd04ec0aa24d265bf92
mattsel <matt.sel04@gmail.com>
2024-05-10 17:47:18 -0400

    Adjusted computer to center of dash

--- static/main.js
+++ static/main.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default anchor behavior
+            const href = this.getAttribute('href');
+            
+            // Check if the link is meant for smooth scrolling within the page
+            if (href.startsWith('#')) {
+                event.preventDefault(); // Prevent default anchor behavior
 
-            const targetId = this.getAttribute('href').substring(1); // Get target section ID
-            const targetSection = document.getElementById(targetId);
+                const targetId = href.substring(1); // Get target section ID
+                const targetSection = document.getElementById(targetId);
 
-            if (targetSection) {
-                // Scroll to the target section with an offset
-                targetSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', scrollMarginTop: 100 });
+                if (targetSection) {
+                    // Scroll to the target section with an offset
+                    targetSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+                }
             }
         });
     });
